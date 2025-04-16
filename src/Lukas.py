87cedd796110b5acf8d3c82014e9a1f9de56e725
Lukas Mayer <146703032+luk1441@users.noreply.github.com>
2025-04-16 12:33:35 +0200

    Lukas tests (#35)

---
+++ src/Lukas.py
@@ -0,0 +1,12 @@
+def add(a: int | float, b: int | float) -> int | float:
+    """
+    Addiert zwei Zahlen (entweder int oder float) und gibt das Ergebnis zurück.
+
+    Parameter:
+    a (int | float): Die erste Zahl.
+    b (int | float): Die zweite Zahl.
+
+    Rückgabewert:
+    int | float: Die Summe der beiden Zahlen.
+    """
+    return a + b
