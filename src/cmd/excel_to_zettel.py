afc2cd03f4afa78c77970b644cc835e1ed4b4e01
luk1441 <lmayer5@stud.hs-heilbronn.de>
2025-05-14 11:11:09 +0200

    added zettel id input to excel_to_zettel.py

--- src/cmd/excel_to_zettel.py
+++ src/cmd/excel_to_zettel.py
@@ -1,4 +1,7 @@
 from src.zettel_excel.excel_to_zettel.main import excel_to_zettel
+import sys
 
 if __name__ == '__main__':
-    print(excel_to_zettel())
+    zettel_id_input = sys.argv[1]
+    result = excel_to_zettel(zettel_id_input)
+    print(result)
