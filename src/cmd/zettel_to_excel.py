0d7f4981c6dfcb8bb77aa3968fd42ab9a86bb5be
Lukas Mayer <146703032+luk1441@users.noreply.github.com>
2025-05-14 11:45:43 +0200

    Fa 01 zettelnummer eingabe (#39)

--- src/cmd/zettel_to_excel.py
+++ src/cmd/zettel_to_excel.py
@@ -1,4 +1,7 @@
 from src.zettel_excel.zettel_to_excel.main import zettel_to_excel
+import sys
 
 if __name__ == '__main__':
-    print(zettel_to_excel())
+    zettel_id_input = sys.argv[1]
+    result = zettel_to_excel(zettel_id_input)
+    print(result)
