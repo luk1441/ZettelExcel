aca9efc67910536747b667d77eacf830a45d087d
maxti <mtimme@stud.hs-heilbronn.de>
2025-05-28 12:44:37 +0200

    #48 mtimme@stud.hs-heilbronn.de funktionen in main.py eingefügt. Hinweis: Müssen noch überarbeitet werden, bsp. Filename.

--- src/zettel_excel/excel_to_zettel/main.py
+++ src/zettel_excel/excel_to_zettel/main.py
@@ -1,2 +1,9 @@
+from src.zettel_excel.excel_to_zettel."""??? Filename ändern. kein Punkt"""
+from src.zettel_excel.excel_to_zettel.sz_to_zettel import embed_table_in_zettel
+from src.zettel_excel.excel_to_zettel.convert_to_sz import to_sz_table
+
 def excel_to_zettel():
-    return "WORLD"
+    table = read_excel.file.py(zettel_id:str)
+    sz_table = to_sz_table(table)
+    zettel = embed_table_in_zettel(sz_table)
+    return zettel
