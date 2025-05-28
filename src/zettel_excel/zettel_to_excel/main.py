8512236d567477d880781f261b86eab291340012
maxti <mtimme@stud.hs-heilbronn.de>
2025-05-28 12:44:09 +0200

    #48 mtimme@stud.hs-heilbronn.de funktionen in main.py eingefügt. Hinweis: Müssen noch überarbeitet werden, bsp. Filename.

--- src/zettel_excel/zettel_to_excel/main.py
+++ src/zettel_excel/zettel_to_excel/main.py
@@ -1,21 +1,15 @@
 from src.zettel_excel.zettel_to_excel.extract_tables import extract_tables
 from src.zettel_excel.zettel_to_excel.get_zettel import get_zettel
-from src.zettel_excel.zettel_to_excel.write_tables_into_excel import write_tables_into_excel
 from src.zettel_excel.zettel_to_excel.zettel_guard import validate_zettelnummer
 from src.zettel_excel.zettel_to_excel.parse_sz import parse_sz
 
 
-def zettel_to_excel(zettel_id: str) -> str:
+def zettel_to_excel(zettel_id: str):
     valid_id = validate_zettelnummer(zettel_id)
-    if valid_id:
+    if valid_id == True:
         body = get_zettel(zettel_id)
-        # tables = extract_tables(body)
-        # TODO: if extract tables works again - uncomment it and delete tables variables defined below
-        tables = [
-            '(TABLE ((CELL (TEXT "name")) (CELL (TEXT "age")) (CELL (TEXT "tabelleintaelle"))) ((CELL (TEXT "name")) (CELL (TEXT "52")) (CELL)))',
-            '(TABLE ((CELL (TEXT "name")) (CELL (TEXT "age"))) ((CELL (TEXT "bran")) (CELL (TEXT "33"))))'
-        ]
+        tables = extract_tables(body)
         parsed_table = parse_sz(tables)
-        return write_tables_into_excel(zettel_id, parsed_table)
     else:
-        raise "Error: Invalid Zettel ID"
+        print("Error: Invalid Zettel ID")
+    return parsed_table
