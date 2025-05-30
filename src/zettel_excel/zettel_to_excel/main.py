b7b5dd30e7e1066179777cb7748a35434fa3f8f5
luk1441 <lukmy3@web.de>
2025-05-30 21:53:54 +0200

    changed zettel_to_excel/main

--- src/zettel_excel/zettel_to_excel/main.py
+++ src/zettel_excel/zettel_to_excel/main.py
@@ -1,15 +1,22 @@
+import logging
+
 from src.zettel_excel.zettel_to_excel.extract_tables import extract_tables
 from src.zettel_excel.zettel_to_excel.get_zettel import get_zettel
-from src.zettel_excel.zettel_to_excel.zettel_guard import validate_zettelnummer
-from src.zettel_excel.zettel_to_excel.parse_sz import parse_sz
+from src.zettel_excel.zettel_to_excel.validate_zettel_id import validate_zettel_id
+from src.zettel_excel.zettel_to_excel.parse_tables import parse_tables
+from src.zettel_excel.zettel_to_excel.write_tables_into_excel import write_tables_into_excel
 
 
-def zettel_to_excel(zettel_id: str):
-    valid_id = validate_zettelnummer(zettel_id)
-    if valid_id == True:
-        body = get_zettel(zettel_id)
-        tables = extract_tables(body)
-        parsed_table = parse_sz(tables)
-    else:
-        print("Error: Invalid Zettel ID")
-    return parsed_table
+def zettel_to_excel(zettel_id: str) -> dict:
+    try:    
+        validate_zettel_id(zettel_id)
+        zettel = get_zettel(zettel_id)
+        encoded_tables = extract_tables(zettel)
+        tables = parse_tables(encoded_tables)
+        message = write_tables_into_excel(zettel_id, tables) 
+        return { "value": message, "error": None }
+    except ValueError as err:
+        return { "value": None, "error": err }
+    except Exception as e:
+        logging.error(f"Unerwarteter Fehler: {e}", exc_info=True)
+        return { "value": None, "error": "Unerwarteter Programmfehler"}
