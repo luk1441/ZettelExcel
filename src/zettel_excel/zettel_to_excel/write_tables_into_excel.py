40ab1b1eefb3f225dcb28db9766576ea9d9df91e
luk1441 <lukmy3@web.de>
2025-06-12 20:33:42 +0200

    fixed write_tables_into_excel.py

--- src/zettel_excel/zettel_to_excel/write_tables_into_excel.py
+++ src/zettel_excel/zettel_to_excel/write_tables_into_excel.py
@@ -9,7 +9,8 @@ def write_tables_into_excel(zettel_id: str, tables: list[pd.DataFrame]) -> str:
 
     with pd.ExcelWriter(output_path, engine="xlsxwriter") as writer:
         for i, table in enumerate(tables):
-            table.to_excel(writer, sheet_name=f"Tabelle {i+1}", index=False)
+            table.to_excel(writer, sheet_name=f"Tabelle {i+1}", index=False,
+                           header=False)
 
     return f"{zettel_id}.xlsx wurde erfolgreich erstellt."
 
