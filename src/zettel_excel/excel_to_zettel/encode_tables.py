d9cbebe46d3096a92de6050b2ed65294dd84dd1d
luk1441 <lukmy3@web.de>
2025-06-16 23:56:52 +0200

    updated encode_tables.py

--- src/zettel_excel/excel_to_zettel/encode_tables.py
+++ src/zettel_excel/excel_to_zettel/encode_tables.py
@@ -4,17 +4,20 @@ import pandas as pd
 def encode_tables(tables: list[pd.DataFrame]) -> str:
     encoded_tables = []
     for table in tables:
-        encoded_tables.append(f"(TABLE {encode_rows(table)})")
-    return " ".join(encoded_tables)
+        encoded_tables.append(encode_rows(table))
+    return "\n---\n".join(encoded_tables)
 
 def encode_rows(table: pd.DataFrame) -> str:
     encoded_rows = []
     for _, row in table.iterrows():
-        encoded_rows.append(f"({encode_cells(row)})")
-    return " ".join(encoded_rows)
+        encoded_rows.append(encode_cells(row))
+    return "\n".join(encoded_rows)
 
 def encode_cells(row: pd.Series) -> str:
     encoded_cells = []
     for cell in row:
-        encoded_cells.append(f"(CELL (){f' (TEXT "{cell}")' if not pd.isna(cell) else ""})")
-    return " ".join(encoded_cells)
+        if pd.isna(cell):
+            encoded_cells.append("")
+        else:
+            encoded_cells.append(f"{cell}")
+    return "|" + "|".join(encoded_cells) + "|"
