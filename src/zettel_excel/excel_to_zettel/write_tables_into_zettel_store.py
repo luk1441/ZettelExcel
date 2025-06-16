e2b3cdf2ac635eaf6c006bb37b1f1a5eb6eb8042
luk1441 <lukmy3@web.de>
2025-06-16 23:39:49 +0200

    updated get_zettel.py

--- src/zettel_excel/excel_to_zettel/write_tables_into_zettel_store.py
+++ src/zettel_excel/excel_to_zettel/write_tables_into_zettel_store.py
@@ -1,24 +1,18 @@
-import os
-import pandas as pd
 import requests
 
 def write_tables_into_zettel_store(zettel_id: str, encoded_tables: str) -> str:
-        print(zettel_id, encoded_tables)
-        """
-        # TODO
-         # PUT-Request vorbereiten
-        url = f"http://localhost:23123/store/{zettelnummer}"
-        updated_zettel = {
-            "title": title,
-            "content": content
-        }
-
-        response = requests.put(url, json=updated_zettel)
-
-        if response.status_code == 200:
-            print(f"Zettel {zettelnummer} erfolgreich aktualisiert.")
-        else:
-            print(f"Fehler bei Zettel {zettelnummer}: {response.status_code}")
-            print(response.text)
-        """
-        return f"Zettel {zettel_id} erfolgreich aktualisiert."
+        base_url = "http://localhost:23123/z/"
+        url = base_url + zettel_id
+        headers = { "Content-Type": "text/plain; charset=utf-8" }
+        meta = f'title: {zettel_id}\nsyntax: zmk'
+        content = encoded_tables
+        data = meta + "\n\n" + content
+        try:
+                response = requests.put(url, data=data,
+                                       headers=headers)
+                if response.status_code == 204:
+                        return f"Zettel {zettel_id} erfolgreich aktualisiert."
+                else:
+                        raise ValueError(response.status_code)
+        except Exception as e:
+                raise ValueError(f"Fehler beim Aktualisieren von Zettel {zettel_id}: {e}")
